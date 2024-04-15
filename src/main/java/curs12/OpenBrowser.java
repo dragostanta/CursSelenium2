f77260539cd2f0d1fb5fd9b29d59d5ebdec0dad6
Dragos Tanta <dragos.tanta@gmail.com>
2024-04-15 09:21:19 +0300

    comit initial

---
+++ src/main/java/curs12/OpenBrowser.java
@@ -0,0 +1,21 @@
+package curs12;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {
+
+	@Test
+	public void testOpenBrowser() {
+		
+		WebDriver driver = new ChromeDriver();//FirefoxDriver()  || EdgeDriver()
+		driver.get("https://keybooks.ro/");
+		
+		
+		
+	}
+	
+	
+	
+}
