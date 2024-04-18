e7deac08ff601cca73a9d8f13c62cb43dd5d3121
Dragos Tanta <dragos.tanta@gmail.com>
2024-04-18 11:35:30 +0300

    curs14 final

--- src/main/java/curs14/SeleniumLocatorsExample.java
+++ src/main/java/curs14/SeleniumLocatorsExample.java
@@ -5,6 +5,7 @@ import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import seleniumutils.BaseTest;
@@ -75,8 +76,46 @@ public class SeleniumLocatorsExample extends BaseTest {
 		
 		System.out.println(price.getText());
 		assertTrue(price.getText().contains("18.49"));
+			
+	}
+	
+	@Test(priority=5)
+	public void idLocator() {
 		
+		Actions action = new Actions(driver);
+		action.scrollByAmount(0, 500).perform();
+		
+		WebElement reviewTab = driver.findElement(By.id("tab-title-reviews"));
+		reviewTab.click();
+		WebElement commentBox =  driver.findElement(By.id("comment"));
+		
+		assertTrue(commentBox.isDisplayed());
 		
 	}
+	@Test(priority=6)
+	public void nameLocator() {
+		
+		WebElement commentBox =  driver.findElement(By.name("comment"));
+		commentBox.sendKeys("My super duper message!");
+	}
+	
+	@Test(priority=7)
+	public void cssLocator() throws InterruptedException {
+		WebElement nameBox = driver.findElement(By.cssSelector("input[name='author']"));
+		nameBox.sendKeys("Johnny Bravo");
+		Thread.sleep(3000);
+		nameBox.clear();
+		Thread.sleep(3000);
+		nameBox.sendKeys("John Doe");
+		Thread.sleep(3000);	
+	}
+	
+	@Test(priority=8)
+	public void xpathLocator() {
+		
+		WebElement emailBox =  driver.findElement(By.xpath("//input[@type='email']"));
+		emailBox.sendKeys("john.doe@test.com");
+	}
+	
 	
 }
